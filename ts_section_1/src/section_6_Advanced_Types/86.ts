4b7ce4e04ab2fbf17aaba86efda405787a598ede
Dragusin Cristian <dragusincristian609@gmail.com>
2022-10-03 20:48:03 +0300

    project setup

--- ts_section_1/src/section_6_Advanced_Types/86.ts
+++
@@ -1,9 +0,0 @@
-//?---------------------------------------------------------
-//? 86: Typecasting
-//?---------------------------------------------------------
-//* 2 ways:
-
-// const userInput7 = document.getElementById("userInput")! as HTMLInputElement
-const userInput7 = <HTMLInputElement>document.getElementById("userInput")!
-
-userInput7.value = "Hi there"
